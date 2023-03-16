5d59885081ad809dd8eaab216c64266cfe523b96
MichaelH616 <michaelhuang6160@gmail.com>
2023-03-16 14:16:28 -0700

    edit section no longer shows up on incorrect id

--- client/src/components/AuthorEdit.jsx
+++ client/src/components/AuthorEdit.jsx
@@ -4,25 +4,25 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const AuthorEdit = () => {
 
-  const { id } = useParams();
-  const navigate = useNavigate();
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-  const [author, setAuthor] = useState({})
+    const [author, setAuthor] = useState({})
 
-  const [ errors, setErrors ] = useState([])
+    const [errors, setErrors] = useState([])
 
-  const [authorNotFoundError, setAuthorNotFoundError] = useState("")
+    const [authorNotFoundError, setAuthorNotFoundError] = useState("")
 
-  useEffect(() => {
-    axios.get(`http://localhost:8000/api/authors/${id}`)
-        .then(res => {
-            setAuthor(res.data);
-        })
-        .catch(err => {
-            console.log(err);
-            setAuthorNotFoundError("We're sorry, but we could not find the author you are looking for. Would you like to add an author to our database?");
-        })
-}, []);
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/authors/${id}`)
+            .then(res => {
+                setAuthor(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+                setAuthorNotFoundError("We're sorry, but we could not find the author you are looking for. Would you like to add an author to our database?");
+            })
+    }, [id]);
 
     const changeHandler = (e) => {
         setAuthor({ ...author, [e.target.name]: e.target.value })
@@ -60,21 +60,26 @@ const AuthorEdit = () => {
             <h1>Favorite authors</h1>
             <div>
                 <Link to='/'>Home</Link>
-                { authorNotFoundError ? <h2 className='my-3 text-center'>{authorNotFoundError} 
-                <br/>
-                <Link to='/new'>Add an author</Link></h2> :  null }
-                <h4 className='text-center'>Edit this author:</h4>
-                {errors.name ? <p className='text-danger text-center'>{errors.name}</p> : ''}
-                <form action='' onSubmit={submitHandler} className='form col-4 offset-4'>
-                    <div className='form-group'>
-                        <label htmlFor="" className='form-label'>Author Name:</label>
-                        <input type='text' name='name' className='form-control text-capitalize' value={author.name}onChange={changeHandler}/>
-                        <Link className='m-2 btn border btn-success' to={'/'}>Cancel</Link>
-                        <button className='btn btn-success'>Submit</button>
+                <br />
+                {!authorNotFoundError ?
+                    <div>
+                        <h4 className='text-center'>Edit this author:</h4>
+                        {errors.name ? <p className='text-danger text-center'>{errors.name}</p> : ''}
+                        <form action='' onSubmit={submitHandler} className='form col-4 offset-4'>
+                            <div className='form-group'>
+                                <label htmlFor="" className='form-label'>Author Name:</label>
+                                <input type='text' name='name' className='form-control text-capitalize' value={author.name} onChange={changeHandler} />
+                                <Link className='m-2 btn border btn-success' to={'/'}>Cancel</Link>
+                                <button className='btn btn-success'>Submit</button>
+                            </div>
+
+                        </form>
                     </div>
 
-                </form>
+                    : <h2 className='my-3 text-center'>{authorNotFoundError} <Link to='/new'>Add an author</Link></h2>
+                }
             </div>
+
         </div>
     )
 }
